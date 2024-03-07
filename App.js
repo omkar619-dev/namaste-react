f8ea19e442ee97e475965b8caad56b4e46f92f15
Omkar <omkarshendge619@gmail.com>
2024-03-08 01:41:16 +0530

    Make some changes in app.js file and jsx introduction

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 
-        const heading = React.createElement("h1",{id:"heading" , xyz:"abc"},"Hello World from React!");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
-    
+  import React from "react";
+  import ReactDOM from "react-dom/client";
+
+  // React Element
+  //React.createElement 
+  const heading = React.createElement("h1",{id:"heading"},"Namaste React");
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading)
+  
+  //JSX we can write React without the need of jsx but it makes our lives easier
+  const jsxheading = (<h1 id="heading">
+    Namaste React using JSX</h1>);
+  root.render(jsxheading)
+  console.log(jsxheading)
